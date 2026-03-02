504080b068cea8ad50b314fc8015deeb54cf678a
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2026-03-02 12:09:09 +0530

    push 05

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,5 +1,5 @@
-const Profile = require('../models/Profile');
-const scrapeGithubProfile = require('../services/scraper');
+const Profile = require("../models/Profile");
+const scrapeGithubProfile = require("../services/scraper");
 
 // POST - Scrape & Save
 exports.scrapeAndSaveProfile = async (req, res) => {
@@ -7,31 +7,27 @@ exports.scrapeAndSaveProfile = async (req, res) => {
     console.log("Incoming Request Body:", req.body);
     const { url } = req.body;
     if (!url) {
-        console.log("❌ No URL Provided");
-        return res.status(400).json({ message: "GitHub URL is required" });
+      console.log("❌ No URL Provided");
+      return res.status(400).json({ message: "GitHub URL is required" });
     }
     const data = await scrapeGithubProfile(url); // Scrape Data
     console.log("📊 Scraped Data:", data);
 
     const savedProfile = await Profile.create(data); // Save to DB
-    console.log("✅ Profile Saved:", savedProfile);
+    console.log("✅ Profile Saved:", savedProfile._id);
 
     res.status(201).json(savedProfile);
-
   } catch (error) {
     console.error("🔥 Error in scrapeAndSaveProfile:", error);
     res.status(500).json({ message: error.message });
   }
 };
 
-
 // GET - Fetch All Profiles
 exports.getAllProfiles = async (req, res) => {
   try {
-    console.log("Incoming Request Body:", req.body);
     const profiles = await Profile.find().sort({ createdAt: -1 });
     res.json(profiles);
-    console.log(profiles);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const profileRoutes = require('./routes/profileRoutes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const profileRoutes = require("./routes/profileRoutes");
 
 const app = express();
-
-connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/profiles', profileRoutes);
+app.use("/api/profiles", profileRoutes);
 
 try {
-    app.listen(5000, () => console.log("Server running on port 5000"));
+  connectDB();
+  app.listen(5000, () => console.log("Server running on port 5000"));
 } catch (error) {
-    console.log("Error To Connect with mongoDB.. ", error)    
+  console.log("Error To Connect with mongoDB.. ", error);
 }
